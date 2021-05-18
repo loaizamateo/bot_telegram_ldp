0748aae2801125311fabcfea2325b1e4557fbd2d
maloai <maloai@bancolombia.com.co>
2021-05-18 09:10:49 -0500

    add dotenv

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
+require('dotenv').config()
+
 const TelegramBot = require('node-telegram-bot-api');
+const token = process.env.TOKEN;
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '1699311562:AAEj8R8-QaWXG56bL7Y0So9TL8uu3NreXQU';
-
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
 bot.onText(/\/inspirame/, msg => {
